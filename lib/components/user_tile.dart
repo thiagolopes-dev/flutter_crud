301abc1ee079c5c0ecabe8ffde203759cf13005d
Thiago Lopes <thiagoolopezz@gmail.com>
2024-03-26 23:27:24 -0300

    Excluir Usuário

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/provider/users.dart';
 import 'package:flutter_crud/routers/app_routes.dart';
+import 'package:provider/provider.dart';
 
 import '../models/user.dart';
 
@@ -27,18 +29,43 @@ class UserTitle extends StatelessWidget {
             children: [
               IconButton(
                 onPressed: () {
-                    Navigator.of(context).pushNamed(
-                AppRoutes.userform,
-                arguments: user,
-              );
+                  Navigator.of(context).pushNamed(
+                    AppRoutes.userform,
+                    arguments: user,
+                  );
                 },
                 icon: const Icon(Icons.edit),
                 color: Colors.orange,
               ),
               IconButton(
-                onPressed: () {},
                 icon: const Icon(Icons.delete),
                 color: Colors.red,
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                      title: const Text('Excluir Usuário'),
+                      content: const Text('Tem certeza?'),
+                      actions: <Widget>[
+                        TextButton(
+                          child: const Text('Não'),
+                          onPressed: () => Navigator.of(context).pop(false),
+                        ),
+                        TextButton(
+                          child: const Text('Sim'),
+                          onPressed: () => Navigator.of(context).pop(true),
+                        ),
+                      ],
+                    ),
+                  ).then((confirmed) {
+                    if (confirmed) {
+                      Provider.of<UsersProvider>(context, listen: false)
+                          .remove(user);
+                    }
+                  });
+                  // Provider.of<UsersProvider>(context, listen: false)
+                  //     .remove(user);
+                },
               )
             ],
           ),
